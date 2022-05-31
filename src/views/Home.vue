43ad2a6eae0e3dc90e6a923b6b8cba69cd8697fc
Ronflex <yann.raelle@viacesi.fr>
2022-05-31 10:09:40 +0200

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
           <div class="container">
             <div class="row">
               <div class="col ">
-                <img class="img-fluid rounded-circle" src="yannraelle.github.io/public/pp.png">
+                <img class="img-fluid rounded-circle" src="pp.png">
               </div>
               <div class="col-8">
                 <div class="row">
